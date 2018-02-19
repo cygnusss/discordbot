0c52234e49d9724da8bdcbbcc03c199516586906
David Kumarbai <cygnidavid@gmail.com>
2018-02-18 19:44:04 -0800

    some changes

--- GoBot/main.go
+++ GoBot/main.go
@@ -1,59 +1,15 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/st3v/translator/microsoft"
 )
 
 // BotID is BotID
 var BotID string
-
-func handleTranslate(t string) string {
-	translator := microsoft.NewTranslator(os.Getenv("MSFT"))
-
-	translation, err := translator.Translate(t, "en", "ru")
-	translation, err = translator.Translate(translation, "ru", "yue")
-	translation, err = translator.Translate(translation, "yue", "vi")
-	translation, err = translator.Translate(translation, "vi", "es")
-	translation, err = translator.Translate(translation, "es", "en")
-
-	if err != nil {
-		log.Panicf("Error during translation: %s", err.Error())
-	}
-	fmt.Printf("TRANSLATION IS: %s", translation)
-	return translation
-}
-
-func handleDadJokes(ch chan string) <-chan string {
-
-	c := &http.Client{}
-	var b DadJokesResponse
-
-	req, err := http.NewRequest("GET", "https://icanhazdadjoke.com/", nil)
-
-	if err != nil {
-		log.Panic(err)
-	}
-
-	req.Header.Add("Accept", "application/json")
-	resp, err := c.Do(req)
-
-	if err := json.NewDecoder(resp.Body).Decode(&b); err != nil {
-		log.Panic(err)
-	}
-
-	defer resp.Body.Close()
-	ch <- b.Joke
-	return ch
-
-}
 
 func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 
@@ -65,26 +21,32 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	dadjBool := strings.HasPrefix(m.Content, "/dadjoke")
 	helpBool := strings.HasPrefix(m.Content, "/donkey")
 
+	c := m.ChannelID
+
 	if helpBool {
-		_, _ = s.ChannelMessageSend("412498257655365633", "'/translate' - translates whatever is after the tag\n'/dadjoke' - get a random dad joke\n¯\\_(ツ)_/¯")
+		_, _ = s.ChannelMessageSend(c, "'/translate' - translates whatever is after the tag into donkey language\n'/dadjoke' - get a random dad joke\n¯\\_(ツ)_/¯")
 	}
 
 	if transBool {
-		_, _ = s.ChannelMessageSend("412498257655365633", handleTranslate(m.Content[11:]))
+		if len(m.Content) <= 11 {
+			_, _ = s.ChannelMessageSend(c, "Message is too short, try again!")
+		} else {
+			_, _ = s.ChannelMessageSend(c, HandleTranslate(m.Content[11:]))
+		}
 	}
 
 	if dadjBool {
 		ch := make(chan string)
-		go handleDadJokes(ch)
-		_, _ = s.ChannelMessageSend("412498257655365633", <-ch)
+		go HandleDadJokes(ch)
+		_, _ = s.ChannelMessageSend(c, <-ch)
 	}
 
 	if strings.Contains(strings.ToLower(m.Content), "bitcoin") {
-		_, _ = s.ChannelMessageSend("412498257655365633", "BITCOIN IS A BUBBLE")
+		_, _ = s.ChannelMessageSend(c, "BITCOIN IS A BUBBLE")
 	}
 
 	if strings.ToLower(m.Content) == "david" {
-		_, _ = s.ChannelMessageSend("412498257655365633", "https://giphy.com/gifs/3djolNOedd5pS")
+		_, _ = s.ChannelMessageSend(c, "https://giphy.com/gifs/3djolNOedd5pS")
 	}
 
 }
